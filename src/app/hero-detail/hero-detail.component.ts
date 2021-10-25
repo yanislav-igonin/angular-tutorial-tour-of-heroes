5125ec2ce1c9974f958d51d014cd822d4d762b24
Yanislav Igonin <yanislavdabest@gmail.com>
2021-10-25 23:26:38 +0300

    feat: move hero detail code to own component

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
 }
