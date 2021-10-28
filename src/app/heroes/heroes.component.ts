ed6a9d0e94bf0c370b17940863899853a409a067
Yanislav Igonin <yanislavdabest@gmail.com>
2021-10-28 17:40:34 +0300

    feat: add hero details on separate route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,20 +9,13 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
   
   constructor(
     private hero: HeroService,
-    private message: MessageService,
   ) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.message.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
