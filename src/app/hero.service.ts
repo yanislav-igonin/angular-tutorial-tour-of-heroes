1bcefab66e58c1791f1c24188461b3819979f6dd
Yanislav Igonin <yanislavdabest@gmail.com>
2021-10-30 17:22:52 +0300

    feat: update get hero method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -50,10 +50,10 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-    // For now, assume that a hero with the specified `id` always exists.
-    // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find(h => h.id === id)!;
-    this.log(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 }
