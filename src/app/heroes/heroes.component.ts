cb1a8310a3d5e6a7a83bb88baecbc40164816b36
Yanislav Igonin <yanislavdabest@gmail.com>
2021-10-30 17:34:41 +0300

    feat: hero can be added now

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,7 +11,7 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   
   constructor(
-    private hero: HeroService,
+    private heroService: HeroService,
   ) { }
 
   ngOnInit(): void {
@@ -19,7 +19,16 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.hero.getHeroes()
+    this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MessageService } from '../message.service';
 
 @Component({
@@ -6,11 +6,7 @@ import { MessageService } from '../message.service';
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent {
 
-  constructor(public message: MessageService) { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(public messageService: MessageService) {}
 }
